30b2d86b14990a7097de43482bfdf13f8e8042f3
bajins <admin@bajins.com>
2019-04-30 16:44:52 +0800

    文件下载api封装

--- src/assets/http.js
+++ src/assets/http.js
@@ -265,14 +265,17 @@ const download = (url, params) => {
             //从response的headers中获取filename, 后端response.setHeader("Content-Disposition", "attachment; filename=xxxx.xxx") 设置的文件名;
             let contentDisposition = result.headers['Content-Disposition'];
             let patt = new RegExp("filename=([^;]+\\.[^\\.;]+);*");
-
+            let filename = "";
             // 如果从Content-Disposition中取到的文件名为空
             if (util.isEmpty(contentDisposition)) {
-                contentDisposition = result.config.params.filePath;
-                patt = new RegExp("([^;]+\\.[^\\.;]+);*");
+                let f = result.config.params.filePath.split("/");
+                filename = f[f.length - 1];
+            } else {
+                filename = patt.exec(contentDisposition)[1];
             }
-            // 取文件名信息中的文件名,替换掉文件名中多余的符号\
-            let filename = util.replace(patt.exec(contentDisposition)[1], "\\\\", "", true);
+            // 取文件名信息中的文件名,替换掉文件名中多余的符号
+            filename = util.replace(filename, "\\\\", "", true);
+            filename = util.replace(filename, "/", "", true);
             let downloadElement = document.createElement('a');
             // 创建下载的链接
             let href = window.URL.createObjectURL(blob);
