16ebcb474085eac263828f117c54528e0947feb3
bajins <admin@bajins.com>
2019-11-29 16:45:56 +0800

    设置代理服务器请求

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,30 @@
+// https://cli.vuejs.org/zh/config/#vue-config-js
 module.exports = {
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false
+    pluginOptions: {
+        i18n: {
+            locale: 'en',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableInSFC: false
+        }
+    },
+    devServer: {
+        open: false,
+        //port: 8000, // 自定义修改8080端口
+        proxy: {
+            // 与Axios.defaults.baseURL的值一至
+            // https://blog.csdn.net/idomyway/article/details/94358986
+            // https://blog.csdn.net/Liu_yunzhao/article/details/90520028
+            '/api': {
+                target: 'http://localhost:8000',
+                // 是否启用websockets
+                ws: true,
+                // secure: false, // https协议才设置
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api/': '/', // rewrite path
+                }
+            }
+        }
     }
-  }
 }
