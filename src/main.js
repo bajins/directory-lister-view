6ec584d5aad627eee01e3659fb63db56e06997e8
bajins <admin@bajins.com>
2019-11-29 16:45:38 +0800

    去除FontAwesomeIcon警告；设置代理服务器请求

--- src/main.js
+++ src/main.js
@@ -1,45 +1,50 @@
-import Vue from 'vue'
-import axios from 'axios'
-import App from './App.vue'
-import router from 'vue-router'
+import Vue from 'vue';
+import Axios from 'axios';
+import App from './App.vue';
+import Routers from './router.js';
 import ViewUI from 'view-design'; // 引入view-design依赖
+import 'view-design/dist/styles/iview.css';
 import VueI18n from 'vue-i18n';
-import 'view-design/dist/styles/view-design.css' // 引入view-design css样式
 import en from 'view-design/dist/locale/en-US';
 import zh from 'view-design/dist/locale/zh-CN';
-import fontawesome from '@fortawesome/fontawesome'
-import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
-import solid from '@fortawesome/fontawesome-free-solid'
-import regular from '@fortawesome/fontawesome-free-regular'
-import brands from '@fortawesome/fontawesome-free-brands'
+import fontawesome from '@fortawesome/fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import solid from '@fortawesome/fontawesome-free-solid';
+import regular from '@fortawesome/fontawesome-free-regular';
+import brands from '@fortawesome/fontawesome-free-brands';
 import config from "./config.js";
 
-Vue.prototype.config=config;
+Vue.prototype.config = config;
 
 fontawesome.library.add(solid);
 fontawesome.library.add(regular);
 fontawesome.library.add(brands);
+
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(VueI18n);
 Vue.use(ViewUI);
-Vue.locale = () => {};
+
+Vue.locale = () => {
+}
+
 const messages = {
-    en: Object.assign({ message: 'hello' }, en),
-    zh: Object.assign({ message: '你好' }, zh)
-};
+    en: Object.assign({message: 'hello'}, en),
+    zh: Object.assign({message: '你好'}, zh)
+}
 
 // Create VueI18n instance with options
 const i18n = new VueI18n({
     locale: 'zh',  // set locale
     messages  // set locale messages
 });
+
 // 1 将所有的主机名和端口 一起设置
-axios.defaults.baseURL = "http://localhost:8090";
+Axios.defaults.baseURL = "/api";
 
 //设置请求的headers
-axios.defaults.headers.common['token'] = ''
-axios.defaults.headers.post['Content-type'] = 'application/json'
+Axios.defaults.headers.common['token'] = '';
+Axios.defaults.headers.post['Content-type'] = 'application/json';
 // axios.defaults.headers.proxy = {
 //     host: "http://localhost",
 //     port: 8090
@@ -49,12 +54,13 @@ axios.defaults.headers.post['Content-type'] = 'application/json'
 // 在 vue 中全局注册 这样可以直接使用不使用 Vue.use() 方式
 // Vue.prototype.$axios = axios
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+//将axios挂载到原型上
+Vue.prototype.$axios = Axios;
 
 new Vue({
-    router,
-    axios,
+    Routers,
+    Axios,
     i18n,
     render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
