3fa28143688f2968be6553415af8456abdc78ad3
bajins <admin@bajins.com>
2019-12-02 16:59:57 +0800

    allows 重新配置全局

--- src/main.js
+++ src/main.js
@@ -1,20 +1,34 @@
+/**
+ * 全局注入配置
+ * https://cn.vuejs.org/v2/api/
+ */
+
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import Axios from 'axios';
 import App from './App.vue';
-import Routers from './router.js';
+import routers from './router.js';
 import ViewUI from 'view-design'; // 引入view-design依赖
 import 'view-design/dist/styles/iview.css';
 import VueI18n from 'vue-i18n';
 import en from 'view-design/dist/locale/en-US';
 import zh from 'view-design/dist/locale/zh-CN';
 import fontawesome from '@fortawesome/fontawesome';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 import solid from '@fortawesome/fontawesome-free-solid';
 import regular from '@fortawesome/fontawesome-free-regular';
 import brands from '@fortawesome/fontawesome-free-brands';
 import config from "./config.js";
 
-Vue.prototype.config = config;
+// 设置为 false 以阻止 vue 在启动时生成生产提示。
+Vue.config.productionTip = false;
+
+// Vue.prototype.config = config;
+Object.defineProperty(Vue.prototype, '$config', {
+    get: function get() {
+        return config
+    }
+});
 
 fontawesome.library.add(solid);
 fontawesome.library.add(regular);
@@ -22,22 +36,20 @@ fontawesome.library.add(brands);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
-Vue.use(VueI18n);
+// https://iviewui.com/docs/guide/install
 Vue.use(ViewUI);
 
-Vue.locale = () => {
-}
-
-const messages = {
-    en: Object.assign({message: 'hello'}, en),
-    zh: Object.assign({message: '你好'}, zh)
-}
-
-// Create VueI18n instance with options
-const i18n = new VueI18n({
+const i18n = {
     locale: 'zh',  // set locale
-    messages  // set locale messages
-});
+    messages: {// set locale messages
+        en: Object.assign({message: 'hello'}, en),
+        zh: Object.assign({message: '你好'}, zh)
+    }
+}
+
+
+Vue.use(VueI18n);
+
 
 // 1 将所有的主机名和端口 一起设置
 Axios.defaults.baseURL = "/home";
@@ -45,22 +57,30 @@ Axios.defaults.baseURL = "/home";
 //设置请求的headers
 Axios.defaults.headers.common['token'] = '';
 Axios.defaults.headers.post['Content-type'] = 'application/json';
-// axios.defaults.headers.proxy = {
-//     host: "http://localhost",
-//     port: 8090
-// }
 
-// 2 将axios添加到Vue的原型对象中
 // 在 vue 中全局注册 这样可以直接使用不使用 Vue.use() 方式
-// Vue.prototype.$axios = axios
+// 将axios挂载到原型上
+Object.defineProperty(Vue.prototype, '$axios', {
+    get: function get() {
+        return Axios
+    }
+});
 
-Vue.config.productionTip = false;
-//将axios挂载到原型上
-Vue.prototype.$axios = Axios;
+// 注入路由
+// https://router.vuejs.org/
+Vue.use(VueRouter);
 
+// 3. 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
+const router = new VueRouter({
+    routers // (缩写) 相当于 routes: routes
+})
+
+
+// 每个Vue应用都是通过用Vue函数创建一个新的根Vue实例开始的
 new Vue({
-    Routers,
-    Axios,
     i18n,
+    router,
+    Axios,
     render: h => h(App)
 }).$mount('#app');
--- src/router.js
+++ src/router.js
@@ -1,30 +1,34 @@
-import Router from 'vue-router';
+/**
+ * 路由
+ */
+
 import Login from './views/login.vue';
 import pageComponent from './components/page.vue';
-import Vue from "vue";
 
-Vue.use(Router);
 
-export default new Router({
-    routes: [
-        {
-            path: '/index',
-            name: 'index',
-            component: pageComponent
-        },
-        {
-            path: '/login',
-            name: 'login1',
-            component: Login
-        },
-        {
-            path: '/login/${path}',
-            name: 'login',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            // component: () => import('./views/login.vue')
-            component: Login
-        }
-    ]
-})
+export default [
+    {
+        path: '/home',
+        name: 'home',
+        component: pageComponent
+    },
+    {
+        path: '/index',
+        name: 'index',
+        component: pageComponent
+    },
+    {
+        path: '/login',
+        name: 'login1',
+        component: Login
+    },
+    {
+        path: '/login/${path}',
+        name: 'login',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        // component: () => import('./views/login.vue')
+        component: Login
+    }
+]
